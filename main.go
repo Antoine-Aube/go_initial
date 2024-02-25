7820c9657c58090e6704082339063fac7679a4d0
Antoine Aube <aaube3@gmail.com>
2024-02-25 11:12:39 -0700

    using types, type inference, and pointers

--- main.go
+++ main.go
@@ -4,20 +4,36 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName string = "Go Conference"
+	conferenceName := "Go Conference"
 	const conferenceTickets int = 50
-	var remainingTickets int = 50
+	var remainingTickets uint = 50
 	
 	fmt.Printf("conferenceTickets is a %T, remainingTickets is %T, confenrenceName is %T\n",conferenceTickets, remainingTickets, conferenceName)//string interpolation
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v are avaiable.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
-	var userTickets int
-	// ask the suer for their name
-	userName = "Tom"
-	userTickets = 2
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+	//user input scan from the fmt library
+	//using pointers - values are stored in memories on your computer - when you reference that value using the variable name
+	// in needs to know that memory address - pointer is a "special variable"
+	fmt.Println("Enter your first name")
+	fmt.Scan(&firstName)
 
-	fmt.Printf("User %v booked %v tickets\n", userName, userTickets)
+	fmt.Println("Enter your last name")
+	fmt.Scan(&lastName)
+	
+	fmt.Println("Enter your email address")
+	fmt.Scan(&email)
+	
+	fmt.Println("Enter number of tickets")
+	fmt.Scan(&userTickets)
+	remainingTickets = remainingTickets - userTickets
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+
+	fmt.Printf("%v tickets remaining for %v", remainingTickets, conferenceName)
 }
