d73bbfe730d64b96f08ddf457fc51f49462e3d3b
Antoine Aube <aaube3@gmail.com>
2024-02-24 17:32:32 -0700

    intial commit

---
+++ main.go
@@ -0,0 +1,10 @@
+//go mod init booking-app
+package main
+//need to declare the execution point - need to give the go compiler a starting point
+import "fmt"
+
+func main() {
+
+	fmt.Print("Hello World")
+
+}
